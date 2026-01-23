81078b43086ab94e7421d2e7806447b34c9a0c75
WilsonRCosta <A43593@alunos.isel.pt>
2026-01-23 16:14:01 +0000

    Update api base path

--- client/src/service/serviceAPI.js
+++ client/src/service/serviceAPI.js
@@ -1,54 +1,60 @@
 import axios from "axios";
 
+const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:8000";
+
+const api = axios.create({
+  baseURL: API_BASE,
+});
+
 const PRODUCTS_URL = "/api/products";
 const USERS_URL = "/api/auth";
 
 export default function clothesService() {
   return {
     // PRODUCT REQUESTS
-    getProducts: () => {
-      return axios.get(PRODUCTS_URL).then(getProductsInfo).catch(getError);
-    },
-    getProductByCode: (code) => {
-      return axios.get(`${PRODUCTS_URL}/${code}`).then(getProductsInfo).catch(getError);
-    },
+    getProducts: () => api.get(PRODUCTS_URL).then(getProductsInfo).catch(getError),
+
+    getProductByCode: (code) =>
+      api.get(`${PRODUCTS_URL}/${code}`).then(getProductsInfo).catch(getError),
+
     createProduct: (product, token) => {
       const { files, ...rest } = product; // remove files
-      return axios
+      return api
         .post(PRODUCTS_URL, rest, { headers: { token } })
         .then(setProductsInfo)
         .catch(getError);
     },
+
     addImageToProduct: (files, code, token) => {
       const formData = new FormData();
-      files.forEach((file) => {
-        formData.append("files", file, file.name);
-      });
-      return axios
-        .put(`${PRODUCTS_URL}/${code}/images`, formData, { headers: { token } })
-        .then(setProductsInfo)
-        .catch(getError);
-    },
-    editProduct: (product, token) => {
-      return axios
-        .put(`${PRODUCTS_URL}/${product.code}`, product, { headers: { token } })
-        .then(setProductsInfo)
-        .catch(getError);
-    },
-    deleteProduct: (code, token) => {
-      return axios
-        .delete(`${PRODUCTS_URL}/${code}`, { headers: { token } })
+      files.forEach((file) => formData.append("files", file, file.name));
+
+      return api
+        .put(`${PRODUCTS_URL}/${code}/images`, formData, {
+          headers: { token },
+        })
         .then(setProductsInfo)
         .catch(getError);
     },
 
+    editProduct: (product, token) =>
+      api
+        .put(`${PRODUCTS_URL}/${product.code}`, product, { headers: { token } })
+        .then(setProductsInfo)
+        .catch(getError),
+
+    deleteProduct: (code, token) =>
+      api
+        .delete(`${PRODUCTS_URL}/${code}`, { headers: { token } })
+        .then(setProductsInfo)
+        .catch(getError),
+
     // AUTHENTICATION REQUESTS
-    loginUser: (user) => {
-      return axios.post(`${USERS_URL}/login`, user).then(getUserInfo).catch(getError);
-    },
-    registerUser: (user) => {
-      return axios.post(`${USERS_URL}/register`, user).then(getUserInfo).catch(getError);
-    },
+    loginUser: (user) =>
+      api.post(`${USERS_URL}/login`, user).then(getUserInfo).catch(getError),
+
+    registerUser: (user) =>
+      api.post(`${USERS_URL}/register`, user).then(getUserInfo).catch(getError),
   };
 }
 
@@ -70,7 +76,7 @@ const setProductsInfo = (resp) => ({
 });
 
 const getError = (err) => ({
-  code: err.response.status,
-  msg: err.response.data.msg,
+  code: err?.response?.status ?? 500,
+  msg: err?.response?.data?.msg ?? err.message,
   type: "error",
 });

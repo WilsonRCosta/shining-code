882726e87bb2df02cc832429464a6ec956380882
Wilson Costa <47864187+WilsonRCosta@users.noreply.github.com>
2026-01-28 16:44:32 +0000

    chore: remove unused static client dist (#10)

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const swaggerDocument = require("./swagger.json");
 const productsController = require("./controller/product-controller.js");
 const usersController = require("./controller/user-controller.js");
 const cors = require("cors");
-const path = require("path");
 
 dotenv.config();
 
@@ -20,14 +19,6 @@ app.use(cors({ origin: [process.env.ORIGIN] }));
 app.use("/api/products", productsController);
 app.use("/api/auth", usersController);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-const clientDistPath = path.join(__dirname, "../client/dist");
-app.use(express.static(clientDistPath));
-
-// --- SPA fallback
-app.get("*", (req, res) => {
-  res.sendFile(path.join(clientDistPath, "index.html"));
-});
 
 mongoose.connect(process.env.DB_CONNECT, (err) =>
   err
